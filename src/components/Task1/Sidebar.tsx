92c7f4aca9a60e677d004f75e34b3fd6c66a0dbf
Prakriti-jain <nonijain740@gmail.com>
2025-04-18 16:24:00 +0530

    added buttons

--- src/components/Task1/Sidebar.tsx
+++ src/components/Task1/Sidebar.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  
   return (
     <aside className="mt-40 text-2xl max-md:mt-10">
       <div className="pl-6 text-black max-md:pl-5">
-        <button className="px-10 py-6 bg-rose-300 bg-opacity-50 rounded-[80px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-md:px-5 w-full text-left">
+        <button
+          onClick={() => navigate("/setpriority")} 
+          className="px-10 py-6 bg-rose-300 bg-opacity-50 rounded-[80px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-md:px-5 w-full text-left">
           Set Priority Level
         </button>
         <button className="px-16 py-6 mt-7 bg-rose-300 bg-opacity-50 rounded-[80px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-md:px-5 w-full text-left">
