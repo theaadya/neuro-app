b5b37c239915e6e92344cb58aae924d453aebacb
Prakriti-jain <nonijain740@gmail.com>
2025-04-21 03:25:20 +0530

    linked social support pages and data privacy pages

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -14,7 +14,7 @@ export const NavigationButton: React.FC<NavigationButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`px-16 py-5 text-3xl text-white bg-rose-300 rounded-3xl transition-transform cursor-pointer border-[none] hover:scale-[1.02] active:scale-[0.98] delay-[0.2s] duration-[0.2s,box-shadow] max-md:px-10 max-md:py-4 max-md:text-2xl max-sm:p-4 max-sm:w-full max-sm:text-xl ${className}`}
+      className={`px-16 py-5 text-3xl text-white bg-rose-300 rounded-3xl transition-transform cursor-pointer border-[none] hover:bg-rose-400 transition-colors duration-200 max-md:px-5" ${className}`}
     >
       {children}
     </button>
